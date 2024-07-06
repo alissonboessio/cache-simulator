98428520a1eab1ce87a7bd0f309f4dae3aeb5ebb
alissonboessio <50459771+alissonboessio@users.noreply.github.com>
2024-07-06 11:34:38 -0300

    Add files via upload

--- simuladorCache.cpp
+++ simuladorCache.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 typedef enum {WriteThrough, WriteBack} PoliticaEscrita;
 typedef enum {LFU, LRU, Aleatoria} PoliticaSubstituicao;
